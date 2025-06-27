9e5f523bd043f6d57eaf4f4ae0a92e4f4b0bebb2
Nathaniel <nathanielng@users.noreply.github.com>
2025-06-27 15:31:37 +0800

    Update create_knowledge_base.py

--- create_knowledge_base.py
+++ create_knowledge_base.py
@@ -55,15 +55,19 @@ def download_file(url):
 
 def extract_zip_file(zip_path):
     try:
+        n_files = 0
         with zipfile.ZipFile(zip_path, 'r') as f:
             file_list = f.namelist()
-            total_files = len(file_list)
-            print(f"Extracting {total_files} files from {zip_path}")
+            print(f"Extracting files from {zip_path}")
             for file in tqdm(file_list, desc="Extracting"):
-                clean_filename = re.sub(r'[\s-]+', '-', file)
-                f.extract(file, f'./{clean_filename}')
-
-        print(f"Successfully extracted {total_files} files")
+                if file.startswith('__') or '.DS_Store' in file:
+                    print(f'Skipping file: {file}')
+                    continue
+                clean_filename = re.sub(r'[\s-]+', '-', file).lower()
+                f.extract(file, '.')
+                os.rename(file, clean_filename)
+                n_files += 1
+        print(f"Successfully extracted {n_files} files")
         return True
     except zipfile.BadZipFile:
         print(f"Error: {zip_path} is not a valid zip file")
@@ -111,7 +115,7 @@ def upload_directory(path, bucket_name):
             print(f"uploading file {file_to_upload} to {bucket_name}")
             s3_client.upload_file(file_to_upload,bucket_name,file)
 
-def create_n_sync_bedrock_knowledge_base(name, description, s3_bucket):
+def create_bedrock_knowledge_base(name, description, s3_bucket):
     knowledge_base = BedrockKnowledgeBase(
         kb_name=name,
         kb_description=description,
@@ -120,13 +124,10 @@ def create_n_sync_bedrock_knowledge_base(name, description, s3_bucket):
     )
     print("Sleeping for 30 seconds.....")
     time.sleep(30)
-    # The following is not required for CUSTOM data sources (but should be added back for S3 data sources)
-    # knowledge_base.start_ingestion_job()
-    knowledge_base_id = knowledge_base.get_knowledge_base_id()
-    data_source_id = knowledge_base.get_datasource_id()
+    return knowledge_base
 
+def ingest_knowledge_base_documents(knowledge_base_id, data_source_id, s3_bucket, kb_folder):
     # Ingest all files in folder into Bedrock Knowledge Base Custom Data Source
-    kb_folder = 'pets-kb-files'
     kb_files = [ file for file in os.listdir(kb_folder) if file.endswith('.pdf') ]
 
     documents = []
@@ -134,7 +135,7 @@ def create_n_sync_bedrock_knowledge_base(name, description, s3_bucket):
         s3_uri = f's3://{s3_bucket}/{kb_file}'
         clean_filename = re.sub(r'[\s-]+', '-', kb_file)
         custom_document_identifier = os.path.splitext(clean_filename)[0]
-        custom_document_identifier = f'pets-{custom_document_identifier.lower()}'
+        custom_document_identifier = custom_document_identifier.lower()
         print(f'{s3_uri} -> Custom Document Identifier: "{custom_document_identifier}"')
         documents.append(
             {
@@ -152,31 +153,70 @@ def create_n_sync_bedrock_knowledge_base(name, description, s3_bucket):
                 }
             }
         )
-
-    response = bedrock_agent_client.ingest_knowledge_base_documents(
-        dataSourceId = data_source_id,
-        documents=documents,
-        knowledgeBaseId = knowledge_base_id
-    )
-    print(json.dumps(response, indent=2, default=str))
-    return knowledge_base_id
+    try:
+        response = bedrock_agent_client.ingest_knowledge_base_documents(
+            dataSourceId = data_source_id,
+            documents=documents,
+            knowledgeBaseId = knowledge_base_id
+        )
+        print(json.dumps(response, indent=2, default=str))
+    except Exception as e:
+        print(f'Exception: {e}')
+        return None
+    return response
 
 
 def main():
-    download_file('https://d2qrbbbqnxtln.cloudfront.net/pets-kb-files.zip')
-    extract_zip_file('pets-kb-files.zip')
-    s3_bucket = create_s3_bucket_with_random_suffix('bedrock-kb-bucket')
-    print(f'Created S3 bucket: {s3_bucket}')
-    upload_directory("pets-kb-files", s3_bucket)
+    folder = 'pets-kb-files'
+    if not os.path.isdir(folder):
+        download_file('https://d2qrbbbqnxtln.cloudfront.net/pets-kb-files.zip')
+        extract_zip_file('pets-kb-files.zip')
+        s3_bucket = create_s3_bucket_with_random_suffix('bedrock-kb-bucket')
+        print(f'Created S3 bucket: {s3_bucket}')
+        upload_directory("pets-kb-files", s3_bucket)
+    else:
+        print('Skipping download as folder {folder} already exists.')
+        buckets = s3_client.list_buckets()['Buckets']
+        s3_buckets = [ b['Name'] for b in buckets if b['Name'].startswith('bedrock-kb-bucket') ]
+        s3_bucket = s3_buckets[0]
 
-    # Create Bedrock KnowledgeBase
-    random_suffix = str(uuid.uuid4())[:8]
-    knowledge_base_id = create_n_sync_bedrock_knowledge_base(
-        name = f'pets-kb-{random_suffix}',
-        description = 'Pets Knowledge Base on cats and dogs',
-        s3_bucket = s3_bucket
+    # Create Bedrock Knowledge Base
+    response = bedrock_agent_client.list_knowledge_bases()
+    knowledge_bases = response.get('knowledgeBaseSummaries')
+    if not len(knowledge_bases):
+        random_suffix = str(uuid.uuid4())[:8]
+        knowledge_base = create_bedrock_knowledge_base(
+            name = f'pets-kb-{random_suffix}',
+            description = 'Pets Knowledge Base on cats and dogs',
+            s3_bucket = s3_bucket
+        )
+        knowledge_base_id = knowledge_base.get_knowledge_base_id()
+        data_source_id = knowledge_base.get_datasource_id()
+        print(f'Created Bedrock Knowledge Base with ID: {knowledge_base_id}')
+    else:
+        print('Skipping Bedrock Knowledge Base Creation')
+        knowledge_base_id = knowledge_bases[0]['knowledgeBaseId']
+        response = bedrock_agent_client.list_data_sources(knowledgeBaseId=knowledge_base_id)
+        data_sources = response['dataSourceSummaries']
+        data_source_ids = [ d['dataSourceId'] for d in data_sources ]
+        if len(data_source_ids):
+            data_source_id = data_source_ids[0]
+        else:
+            print('Error: Data source not created. Please create a custom data source manually')
+            return
+
+    print(f'Loading documents into Bedrock Knowledge Base: {knowledge_base_id}')
+    print(f'Data Source ID: {data_source_id}')
+
+    # Ingest documents from S3 into Bedrock Knowledge Base
+    # Requires the appropriate S3 bucket permissions in the
+    # Knowledge Base role: AmazonBedrockExecutionRoleForKnowledgeBase_xx 
+    ingest_knowledge_base_documents(
+        knowledge_base_id = knowledge_base_id,
+        data_source_id = data_source_id,
+        s3_bucket = s3_bucket,
+        kb_folder = folder
     )
-    print(f'Created Bedrock Knowledge Base with ID: {knowledge_base_id}')
 
 if __name__ == '__main__':
     main()
